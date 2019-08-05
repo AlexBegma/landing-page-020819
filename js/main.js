177c3abbd35d0e3fd21fa13f75fbdd789813b07c
alexbehma <alexbegma@gmail.com>
2019-08-05 17:12:30 +0300

    landing-page without @media

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('.slider__inner').slick({
+		nextArrow: '<input type="radio" class="slick-radio slick-next"></input>',
+		prevArrow: '<input type="radio" class="slick-radio slick-prev"></input>',
+		// infinite: false,
+	});
+
+
+});
